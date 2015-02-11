3621212db792473ea4c4c8e864a6b132b8b386d6
momoiroshikibu <momoiroshikibu@gmail.com>
2015-02-11 12:22:07 +0900

    Just shipped

---
+++ app/src/androidTest/java/momoiroshikibu/com/age34/ApplicationTest.java
@@ -0,0 +1,13 @@
+package momoiroshikibu.com.age34;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
